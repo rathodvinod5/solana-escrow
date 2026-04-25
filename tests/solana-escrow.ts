449b218a2ebec9e466c26746651c37198ce88763
rathodvinod5 <rathodvinod5@gmail.com>
2026-04-25 12:23:44 +0530

    added basic structure for test cases

--- tests/solana-escrow.ts
+++ tests/solana-escrow.ts
@@ -1,16 +1,46 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaEscrow } from "../target/types/solana_escrow";
+import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
 describe("solana-escrow", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  // anchor.setProvider(anchor.AnchorProvider.env());
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.solanaEscrow as Program<SolanaEscrow>;
+  const admin = anchor.web3.Keypair.generate();
+  const maker = anchor.web3.Keypair.generate();
+  const taker = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  let tokenMintA = PublicKey;
+  let tokenMintB = PublicKey;
+  let makerAtaForTokenA = PublicKey;
+  let makerAtaForTokenB = PublicKey;
+  let takerAtaForTokenA = PublicKey;
+  let takerAtaForTokenB = PublicKey;
+  let vault = PublicKey;
+  let escrowOffer = PublicKey;
+  let escrowSeeds;
+
+  describe("AIRDROP", async () => {
+    await airdrop(provider.connection, admin.publicKey, 10 * LAMPORTS_PER_SOL);
+    await airdrop(provider.connection, maker.publicKey, 10 * LAMPORTS_PER_SOL);
+    await airdrop(provider.connection, taker.publicKey, 10 * LAMPORTS_PER_SOL);
+  });
+
+  describe("CREATE TOKEN MINTS", async () => {
+    it("should create tokenMintA", async () => {
+      // tokenMintA = await createMint();
+    });
   });
 });
+
+async function airdrop(connection: any, address: any, amount = 1000000000) {
+  await connection.confirmTransaction(
+    await connection.requestAirdrop(address, amount),
+    "confirmed",
+  );
+}
